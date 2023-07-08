d334439759e441ee100c5f9b81d09538e9289e7f
santiagomnr <santiagomnr.blak@gmail.com>
2023-07-07 21:39:56 -0300

    updateparlaentrega

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -1,34 +1,27 @@
-import { ADD_FAV, REMOVE_FAV, FILTER } from "./actions-types";
+import { ADD_FAV, REMOVE_FAV } from "./actions-types";
 
 const initialState = {
-    myFavorites: [],
-    allCharacter: []
+    myFavorite: []
 }
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case ADD_FAV:
-            const copyAllCharacter = state.allCharacter;
-                return {
-                    ...state,
-                    myFavorites: copyAllCharacter,
-                    allCharacter: copyAllCharacter
-                }
-        case FILTER:
-          return {
-            ...state
-          }  
-            
-
-        case REMOVE_FAV:
-            let deleteCharacter = state.myFavorites.filter(character => character.id !== Number(action.payload))
             return {
                 ...state,
-                myFavorites: deleteCharacter
+                myFavorite: [...state.
+                myFavorite, action.payload]
             }
+        case REMOVE_FAV:
+            let deletedCharacter = state.
+            myFavorite.filter(character =>
+                character.id !== Number(action.
+                payload))
+                return {
+                    ...state,
+                    myFavorite: deletedCharacter
+                }
 
-
-        
         default:
             return {
                 ...state
@@ -37,3 +30,68 @@ const reducer = (state = initialState, action) => {
 }
 
 export default reducer;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { ADD_FAV, REMOVE_FAV, FILTER } from "./actions-types";
+
+// const initialState = {
+//     myFavorites: [],
+//     allCharacter: []
+// }
+
+// const reducer = (state = initialState, action) => {
+//     switch (action.type) {
+//         case ADD_FAV:
+//             const copyAllCharacter = state.allCharacter;
+//                 return {
+//                     ...state,
+//                     myFavorites: copyAllCharacter,
+//                     allCharacter: copyAllCharacter
+//                 }
+//         case FILTER:
+//           return {
+//             ...state
+//           }  
+            
+
+//         case REMOVE_FAV:
+//             let deleteCharacter = state.myFavorites.filter(character => character.id !== Number(action.payload))
+//             return {
+//                 ...state,
+//                 myFavorites: deleteCharacter
+//             }
+
+
+        
+//         default:
+//             return {
+//                 ...state
+//             }
+//     }
+// }
+
+// export default reducer;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,13 +1,65 @@
-import { createStore, applyMiddleware, compose } from "redux"; 
-import thunkMiddleware from "redux-thunk";
-// import { composeWithDevTools,  } from 'redux-devtools-extension';
-import rootReducer from "./reducer";
-
-const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+import { createStore, applyMiddleware } from "redux"; 
+import thunk from "redux-thunk";
+import { composeWithDevTools  } from 'redux-devtools-extension';
+import reducer from "./reducer";
 
 const store = createStore(
-  rootReducer,
-  composeEnhancer(applyMiddleware(thunkMiddleware))
+    reducer,
+    composeWithDevTools(applyMiddleware(thunk))
 );
 
 export default store;
+
+
+
+
+
+
+
+
+
+
+// import { createStore, applyMiddleware, compose } from "redux"; 
+// import thunkMiddleware from "redux-thunk";
+// import { composeWithDevTools,  } from 'redux-devtools-extension';
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import { createStore, applyMiddleware, compose } from "redux"; 
+// import thunkMiddleware from "redux-thunk";
+// import { composeWithDevTools,  } from 'redux-devtools-extension';
+// import rootReducer from "./reducer";
+
+// const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE || compose;
+
+// const store = createStore(
+//   rootReducer,
+//   composeEnhancer(applyMiddleware(thunkMiddleware))
+// );
+
+// export default store;
