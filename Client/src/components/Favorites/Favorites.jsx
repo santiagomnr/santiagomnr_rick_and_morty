e6d95aac408a2858e7ca448c9544209e269c2f0d
santiagomnr <santiagomnr.blak@gmail.com>
2023-07-21 18:39:26 -0300

    updateV3.5.0

--- Client/src/components/Favorites/Favorites.jsx
+++ Client/src/components/Favorites/Favorites.jsx
@@ -1,12 +1,35 @@
 import { connect } from "react-redux";
 import Card from "../Card/Card";
+import { filterCards, orderCards } from "../../redux/actions";
+import { useDispatch } from "react-redux";
+import { useState } from "react";
 
 const Favorites = (myFavorites) => {
-  // const { myFavorites } = props;
+  const [ aux, setAux ] = useState(false);
+  const dispatch = useDispatch();
+  
+  const handleOrder = (e) => {
+    dispatch(orderCards(e.target.value))
+    setAux(!aux)
+  }
+
+  const handleFilter = (e) => {
+    dispatch(filterCards(e.target.value))
+  }
 
   return (
     <div>
       <h1>My Favorites 💘</h1>
+      <select onChange={handleOrder}> 
+        <option value="A">Ascendant</option>
+        <option value="D">Descending</option>
+      </select>
+      <select onChange={handleFilter}>
+        <option value="Male">Male</option>
+        <option value="Famale">Famale</option>
+        <option value="GenderLess">GenderLess</option>
+        <option value="unknown">unknown</option>
+      </select>
       {
         myFavorites?.map(character => {
           return (
--- Client/src/redux/reducer.js
+++ Client/src/redux/reducer.js
@@ -1,17 +1,47 @@
-import { ADD_FAV, REMOVE_FAV } from "./actions-types";
+import { ADD_FAV, REMOVE_FAV, FILTER, ORDER } from "./actions-types";
 
 const initialState = {
-  myFavorite: []
+  myFavorite: [],
+  allCharacter:[]
 }
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ADD_FAV:
+      const copyAllCharacter = [ ...state.allCharacter, action.payload];
       return {
         ...state,
-        myFavorite: [...state.
-        myFavorite, action.payload]
+        myFavorite: copyAllCharacter,
+        allCharacter: [ ...copyAllCharacter ]
+      };
+
+    case FILTER:
+      const copyFilter = state.allCharacter.filter(
+        (character) => character.gender === action.payload
+      )
+      return {
+        ...state,
+        myFavorite: copyFilter
       }
+    
+    case ORDER:
+      const orderCharacter = state.allCharacter.
+      sort((a, b) => {
+        if(action.payload === "A") {
+          if(a.id < b.id) return -1;
+          if(b.id < a.id) return 1;
+          return 0;
+        } else {
+          if(a.id < b.id) return 1;
+          if(b.id < a.id) return -1;
+          return 0;  
+        }
+      })
+      return {
+        ...state,
+        myFavorite: [...orderCharacter]
+      };
+
     case REMOVE_FAV:
       const deletedCharacter = state.
       myFavorite.filter(
@@ -20,7 +50,8 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         myFavorite: deletedCharacter
-      }
+      };
+      
     default:
       return {
         ...state
